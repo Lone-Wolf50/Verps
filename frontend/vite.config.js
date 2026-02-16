5b59fcab2036da33e8da12949fbeca0e376be297
lone wolf <speedforce678@gmail.com>
2026-02-16 22:45:42 +0000

    fixed config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { tanstackRouter } from "@tanstack/router-plugin/vite"; // Or TanStackRouterVite
 
+// https://vite.dev/config/
 export default defineConfig({
-	plugins: [
-		tanstackRouter(), // Make sure this is BEFORE react()
-		react(),
-	],
+	plugins: [react()],
 });
