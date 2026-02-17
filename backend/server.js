0f879dc341ac0697f92699c9c47eb5f237828da2
lone wolf <speedforce678@gmail.com>
2026-02-17 01:57:33 +0000

    updating server

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 const allowedOrigins = [
 	"http://localhost:3000",
 	"http://localhost:5173",
-	"https://verps-client.vercel.app/",
+	"https://verps-client.vercel.app",
 ];
 
 app.use(
