6ee088296d136236a1c27650a2fb1579a1377bdd
lone wolf <speedforce678@gmail.com>
2026-02-24 01:36:14 +0000

    still on path issues

--- frontend/src/MercComponents/Administration/AddProduct.jsx
+++ frontend/src/MercComponents/Administration/AddProduct.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { supabase } from "../supabaseClient";
 import Swal from "sweetalert2";
-import { API_URL } from "../../../config.js";
+import { API_URL } from "../../config.js";
 import { Trash2, UploadCloud } from "lucide-react";
 
 const AddProduct = ({ editData, onSuccess }) => {
